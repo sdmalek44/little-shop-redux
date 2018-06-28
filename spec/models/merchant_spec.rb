d173b4067019ccf68cc4801d44678fe52f0deef1
MacInnes <califormula@gmail.com>
2018-06-28 15:01:33 -0600

    some testing complete

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,34 @@ RSpec.describe Merchant do
       expect(merchant).to_not be_valid
     end
   end
+  describe 'When user is' do
+    context 'Visiting /merchants' do
+      it 'can create, edit and delete a new merchant' do
+        visit '/merchants'
+
+        click_link('Create a new Merchant')
+        expect(current_path).to eq('/merchants/new')
+
+        fill_in('merchant[name]', with: 'example Merchant name')
+        click_on('Submit')
+        expect(current_path).to eq('/merchants')
+
+        within('.merchant-name') do
+          expect(page).to have_content('name: example Merchant name')
+        end
+        visit '/merchants'
+
+        click_on('edit')
+        expect(current_path).to eq('/merchants/1/edit')
+
+        fill_in('merchant[name]', with: 'a different merchant name')
+        click_on('Edit')
+        expect(current_path).to eq('/merchants')
+        within('.merchant-name') do
+          expect(page).to have_content('name: a different merchant name')
+        end
+      end
+
+    end
+  end
 end
