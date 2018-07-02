ebd2b1462abd90d1aec6abecef1a67571016ed8b
Seth Schwartz <sschwartz214@gmail.com>
2018-07-02 15:57:10 -0600

    adds test for merchant with highest item price

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,17 +8,31 @@ RSpec.describe Item do
     it 'is invalid without a description' do
       item = Item.create(title: 'puzzle', price: 1000, image:'/example')
 
-    expect(item).to_not be_valid
+      expect(item).to_not be_valid
     end
     it 'is invalid without a price' do
       item = Item.create(title: 'puzzle', description: '300 piece jigsaw', image:'/example')
 
-    expect(item).to_not be_valid
+      expect(item).to_not be_valid
     end
     it 'is invalid without an image' do
       item = Item.create(title: 'puzzle', description: '300 piece jigsaw', price: 1000)
 
-    expect(item).to_not be_valid
+      expect(item).to_not be_valid
+    end
+  end
+  describe 'Class Methods' do
+    it 'finds merchant with highest priced item' do
+      # merchant1 = Merchant.items.create(title: 'puzzle', description: '300 piece jigsaw', price: 1000)
+
+      merchant1 = Merchant.create(name: "Bob")
+      item1 = Item.create(merchant_id: 1, title: 'puzzle', description: '300 piece jigsaw', price: 1000, image: "google.com")
+      merchant2 = Merchant.create(name: "Joe")
+      item2 = Item.create(merchant_id: 2, title: 'puzzle', description: '300 piece jigsaw', price: 2000, image: "google.com")
+
+      expected_result = merchant2
+
+      expect(Item.merchant_with_highest_item_price).to eq(expected_result)
     end
   end
 end
